6b5a60c3eeb2b888ca513dfa8376910c96371b19
Thao Truong <ttngocthao_87@yahoo.com>
2022-01-26 10:50:33 +0000

    completed 10.11

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ import knexfile from '../knexfile';
 
 dotenv.config({ path: path.resolve(__dirname, '..', '.env') });
 
-export const API_PORT = process.env.PORT || 5000;
+export const API_PORT = process.env.PORT || 15000;
 
 export const APOLLO_PORT = process.env.APOLLO_PORT || 4000;
 
